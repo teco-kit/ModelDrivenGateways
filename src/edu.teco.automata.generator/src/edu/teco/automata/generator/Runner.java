4ff1bd231e88c4cccf55e52032fdc357a42bd399
Till Riedel <riedel@teco.edu>
2009-03-27 15:43:23 +0000

    changed Runner to be more simple/modular

--- src/edu.teco.automata.generator/src/edu/teco/automata/generator/Runner.java
+++ src/edu.teco.automata.generator/src/edu/teco/automata/generator/Runner.java
@@ -1,68 +1,86 @@
 package edu.teco.automata.generator;
 
 import java.io.File;
-import java.util.Collection;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.xsd.ecore.XSDEcoreBuilder;
 import org.openarchitectureware.workflow.WorkflowRunner;
 import org.openarchitectureware.workflow.monitor.NullProgressMonitor;
 
+
 public class Runner {
-   public static void main(String args[]) {
-      String xsdPath = args[0];
-      String genDir  = args[1];
+    public static void main(String xsdPath,String genDir,String[] target)
+    {
+    	final String wfFileAutomata="src/edu/teco/automata/generator/xsdautomata.oaw";
+        final String wfFileTargetPrefix="src/edu/teco/automata/generator/targets/";
+        final String wfFileTargetSuffix=".oaw";
+        
+        for(String t:target)
+        {
+        if(!(new File(wfFileTargetPrefix+t+wfFileTargetSuffix)).exists())
+        {
+      	  System.out.println("target " + wfFileTargetPrefix+t+wfFileTargetSuffix+ " doesn't exist");
+      	  System.exit(1);
+        }
+        }
+        
+        Map<String, EPackage> slotContents = new HashMap<String, EPackage>();
+        Map<String, String> properties = new HashMap<String, String>();
+        
+        java.util.Properties  propertiesFile = new java.util.Properties();
+        
+        File p=new File("generator.properties");
+        if(p.exists())
+        {
+        	
+        try {
+			propertiesFile.load(new FileReader(p));
+			for(Entry<Object, Object> e:propertiesFile.entrySet())
+				properties.put((String)e.getKey(), (String)e.getValue());
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+        
+        }
+        
+        org.openarchitectureware.workflow.monitor.ProgressMonitor ProgressMonitor=new NullProgressMonitor();
+       
+        if(xsdPath!=null)
+        	properties.put("schemaFile", xsdPath);
+        
+        if(genDir!=null)
+        	properties.put("outlet_path", genDir);
+        
+        properties.put("outputSlot", "model" );
+        
+        WorkflowRunner r=new WorkflowRunner();
+        if(!r.run(wfFileAutomata, ProgressMonitor,
+                properties, slotContents)) System.exit(1);
+        for(String t:target)
+        {
+        	if(!r.run(wfFileTargetPrefix+t+wfFileTargetSuffix, ProgressMonitor,
+                properties, slotContents)) System.exit(1);
+        }
+        
+    }
+    
+	public static void main(String args[]) {
       
-      File xsdFile = new File(xsdPath);
-      if (!xsdFile.exists()) {
-         System.out.println("File doesn't exist");
-         return;
-      }
-
-      XSDEcoreBuilder xsdEcoreBuilder = new XSDEcoreBuilder();
-      Collection<EObject> eCorePackages = xsdEcoreBuilder.generate(URI
-            .createFileURI(xsdPath));
-
-      String wfFileSchema = "edu/teco/automata/generator/m2mt.oaw";
-      Map<String, String> propertiesSchema = new HashMap<String, String>();
-      Map<String, EPackage> slotContents = new HashMap<String, EPackage>();
-
-      for (Iterator<EObject> iter = eCorePackages.iterator(); iter.hasNext();) {
-         EPackage element = (EPackage) iter.next();
-         slotContents.put("SchemaModel", element);
-      }
-      int sufIdx = xsdFile.getName().lastIndexOf(".");
-      String automataFileName = xsdFile.getName().substring(0, sufIdx) +
-                                ".automata";  
-
-      // propertiesSchema.put("modelFile", "platform:/resource"
-      // + selectedFile.getFullPath());
-      propertiesSchema.put("srcgen", genDir);
-
-      new File(genDir).mkdirs();
-      propertiesSchema.put("automataFile", automataFileName);
-
-      new WorkflowRunner().run(wfFileSchema, new NullProgressMonitor(),
-            propertiesSchema, slotContents);
-
-      String wfFileAutomata = "edu/teco/automata/generator/workflow.oaw";
-      Map<String, String> properties = new HashMap<String, String>();
-      Map<String, EPackage> slotContents2 = new HashMap<String, EPackage>();
-
-      properties.put("srcgen", genDir);
-      properties.put("modelFile", genDir + "/" + automataFileName);
-//         properties.put("DeviceName", config.getDeviceName());
-//         properties.put("NSPrefix", config.getNsPrefix());
-//         properties.put("MsgOutURI", config.getMsgOutURI());
-//         properties.put("MsgRspURI", config.getMsgRspURI());
-
-      new WorkflowRunner().run(wfFileAutomata, new NullProgressMonitor(),
-            properties, slotContents2);
+	  if(args.length<3)
+	  {
+      	  System.out.println("usage: Runner xsdPath genDir target*");
+      	  System.exit(1);
+	  }
+	  
+      main(args[0],args[1],Arrays.copyOfRange(args, 2, args.length));
 
    }
 }
