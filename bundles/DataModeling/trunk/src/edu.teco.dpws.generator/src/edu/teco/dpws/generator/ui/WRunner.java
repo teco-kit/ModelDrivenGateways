063d09064d706f0e909432fb1d1fabcc0916655e
riedel <riedel@8558c0a7-2cd5-0310-9c60-ed1042e00004>
2009-08-20 16:30:54 +0000

    Small bugfixes. Now work in foreign environment.

--- bundles/DataModeling/trunk/src/edu.teco.dpws.generator/src/edu/teco/dpws/generator/ui/WRunner.java
+++ bundles/DataModeling/trunk/src/edu.teco.dpws.generator/src/edu/teco/dpws/generator/ui/WRunner.java
@@ -6,11 +6,9 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IPathVariableManager;
@@ -36,9 +34,6 @@ import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-
-
-
 public class WRunner extends AbstractHandler implements IActionDelegate {
 
 	IFile selectedFile = null;
@@ -46,9 +41,9 @@ public class WRunner extends AbstractHandler implements IActionDelegate {
 	String baseName;
 	String dirName;
 	String workspaceURL;
-	
+
 	String wfFile = "wsdl.oaw";
-	
+
 	/**
 	 * Constructor for Action1.
 	 */
@@ -70,73 +65,80 @@ public class WRunner extends AbstractHandler implements IActionDelegate {
 	public void run(IAction action) {
 		run();
 	}
-	
-	@SuppressWarnings("unchecked")
-	public void run(){
 
-		
+	@SuppressWarnings("unchecked")
+	public void run() {
+
 		final Map propertiesSchema = new HashMap();
-		
+
 		propertiesSchema.put("dirName", dirName);
 		propertiesSchema.put("baseName", baseName);
 		propertiesSchema.put("platformURI", workspaceURL);
-		Plugin.getDefault().getLog().log(new Status(IStatus.INFO,
-			      Plugin.PLUGIN_ID, 0,
-			      "Generating " +baseName, null));
-		
+		Plugin.getDefault().getLog().log(
+				new Status(IStatus.INFO, Plugin.PLUGIN_ID, 0, "Generating "
+						+ baseName, null));
+
 		Log.setLog(Plugin.getDefault().getLog());
-		
-	    Job job = new Job("Generating code for "+ baseName) {
-			     protected IStatus run(IProgressMonitor monitor) {
-			    	 
-			 		  Thread.currentThread().setContextClassLoader(WRunner.class.getClassLoader());
-					  System.setProperty("org.apache.commons.logging.Log", Log.class.getName());
-			          return new WorkflowRunner().run(wfFile, new ProgressMonitorAdapter(monitor),
-			   				propertiesSchema, null)?Status.OK_STATUS:Status.CANCEL_STATUS; 
-			          
-			        }
-			     };
-			  job.setPriority(Job.BUILD);
-			  job.schedule();
+
+		Job job = new Job("Generating code for " + baseName) {
+			protected IStatus run(IProgressMonitor monitor) {
+
+				Thread.currentThread().setContextClassLoader(
+						WRunner.class.getClassLoader());
+				System.setProperty("org.apache.commons.logging.Log", Log.class
+						.getName());
+				return new WorkflowRunner().run(wfFile,
+						new ProgressMonitorAdapter(monitor), propertiesSchema,
+						null) ? Status.OK_STATUS : Status.CANCEL_STATUS;
+
+			}
+		};
+		job.setPriority(Job.BUILD);
+		job.schedule();
 	}
-    
+
 	/**
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
-	
-	private void setVars(IStructuredSelection selection)
-	{
+
+	private void setVars(IStructuredSelection selection) {
 		IFile f = (IFile) selection.getFirstElement();
-		baseName=f.getLocation().removeFileExtension().lastSegment();
-		wfFile=f.getLocation().getFileExtension()+".oaw";
-		dirName=f.getLocation().removeLastSegments(1).toPortableString();
-		workspaceURL=ResourcesPlugin.getWorkspace().getRoot().getLocation().toPortableString();	
+		baseName = f.getLocation().removeFileExtension().lastSegment();
+		wfFile = f.getLocation().getFileExtension() + ".oaw";
+		dirName = f.getLocation().removeLastSegments(1).toPortableString();
+		workspaceURL = ResourcesPlugin.getWorkspace().getRoot().getLocation()
+				.toPortableString();
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IPathVariableManager pathMan = workspace.getPathVariableManager();
-		
-		URL url=ResourceLoaderFactory.createResourceLoader().getResource("shared");
-		try
-		{
-		try{
-			URL newURL=FileLocator.toFileURL(url);
-			url=newURL;
-		}
-		catch(NullPointerException e)
-		{
-		}
-		
-		String pathStr=new File(url.getPath()).getCanonicalPath();
-		IPath value = new Path(pathStr);
-		
-		String name=Plugin.PLUGIN_ID.replace("\\.","_").toUpperCase()+"SHARED";
-		
-		if (pathMan.validateName(name).isOK() && pathMan.validateValue(value).isOK()) {
-		      pathMan.setValue(name, value);
-		}
-		}catch(IOException e){} catch (CoreException e) {}
 
-		
+		URL url = ResourceLoaderFactory.createResourceLoader().getResource(
+				"shared");
+		try {
+			try {
+				URL newURL = FileLocator.toFileURL(url);
+				url = newURL;
+			} catch (NullPointerException e) {
+			}
+
+			String pathStr = new File(url.getPath()).getAbsolutePath();
+			IPath value = new Path(pathStr);
+
+			String name = Plugin.PLUGIN_ID.replaceAll("\\.", "_").toUpperCase()
+					+ "_SHARED";
+
+			if (pathMan.validateName(name).isOK()
+					&& pathMan.validateValue(value).isOK()) {
+				pathMan.setValue(name, value);
+			}
+			else
+			{
+				throw new IOException("\""+name + "\":" +pathMan.validateName(name).getMessage()+"\n"+"\""+value + "\":" +pathMan.validateValue(value).getMessage());
+			}
+		} catch (Exception e) {
+			Plugin.getDefault().getLog().log(new Status(IStatus.WARNING, Plugin.PLUGIN_ID, 0, "Failed to set shared source folder", e));
+		} 
 	}
+
 	public void selectionChanged(IAction action, ISelection selection) {
 
 		if (selection instanceof IStructuredSelection) {
@@ -145,7 +147,7 @@ public class WRunner extends AbstractHandler implements IActionDelegate {
 	}
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ISelection selection=HandlerUtil.getCurrentSelection(event);
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
 		if (selection instanceof IStructuredSelection) {
 			setVars((IStructuredSelection) selection);
 			run();
