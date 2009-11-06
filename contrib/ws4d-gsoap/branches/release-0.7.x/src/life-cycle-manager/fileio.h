e8fc140f9f9706b0bbb9d0bb6b2dfbe453ff9227
Till Riedel <riedel@teco.edu>
2009-11-06 16:57:22 +0000

    rather use trunk

--- contrib/ws4d-gsoap/branches/release-0.7.x/src/life-cycle-manager/fileio.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int readfile(const char* filename, unsigned char* bytes);
-int writefile(const char* filename, unsigned char* bytes, size_t len);
-int comparefiles(const char* filename1, const char* filename2);
-int readintfromfile(const char* filename);
-int writeinttofile(int val, const char* filename);
