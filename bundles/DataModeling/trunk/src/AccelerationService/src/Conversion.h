4e726501b8003495012552424864ac6f664a1472
wildschu <wildschu@8558c0a7-2cd5-0310-9c60-ed1042e00004>
2011-01-23 21:22:40 +0000

    Changed message format to include delta Prepared message for data logging service (use series element containing samples instead of single sample)

--- bundles/DataModeling/trunk/src/AccelerationService/src/Conversion.h
+++ bundles/DataModeling/trunk/src/AccelerationService/src/Conversion.h
@@ -10,6 +10,6 @@ void writeSOAPBuf(struct soap *soap,char * buf);
 /**
  * Write acceleration values to SOAP output
  */
-void writeSOAPValues(struct soap *soap,float x,float y,float z);
+void writeSOAPValues(struct soap *soap,float x,float y,float z,float tick, float delta);
 
 #endif /* CONVERSION_H_ */
