0aee0eae739ce92d8dfb53f05ec02943d56946d4
wildschu <wildschu@8558c0a7-2cd5-0310-9c60-ed1042e00004>
2011-02-17 14:17:57 +0000

    Bugfixing

--- bundles/DataModeling/trunk/src/DeviceInfoService/src/Conversion.c
+++ bundles/DataModeling/trunk/src/DeviceInfoService/src/Conversion.c
@@ -18,12 +18,21 @@
 
 void writeDeviceInfo(struct soap *soap,DeviceInfo * info)
 {
-	soap_element_begin_out(soap, "dvcinf:deviceinfo", 0, "");
+	soap_element_begin_out(soap, "dvcinf:status", 0, "");
 
 	char * status_str = soap_strdup(soap, info->status);
-	soap_outstring(soap, "dvcinf:status", -1, &status_str, "", SOAP_TYPE_string);
+	soap_outstring(soap, "dvcinf:description", -1, &status_str, "", SOAP_TYPE_string);
+	if(strcmp("Idle",info->status)==0)
+	{
+		char * ready_str = soap_strdup(soap, "true");
+		soap_outstring(soap, "dvcinf:ready", -1, &ready_str, "", SOAP_TYPE_string);
+	} else {
+		char * ready_str = soap_strdup(soap, "false");
+		soap_outstring(soap, "dvcinf:ready", -1, &ready_str, "", SOAP_TYPE_string);
+	}
 
-	soap_element_end_out(soap, "dvcinf:deviceinfo");
+
+	soap_element_end_out(soap, "dvcinf:status");
 }
 
 void writeStopDevice(struct soap *soap,char * buf,int len)
