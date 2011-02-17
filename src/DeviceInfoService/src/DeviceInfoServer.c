6dd7ccd5554868a4abbe986e4aed82899fc25c7c
wildschu <5690878+dlwildschut@users.noreply.github.com>
2011-02-17 14:17:57 +0000

    Bugfixing

--- src/DeviceInfoService/src/DeviceInfoServer.c
+++ src/DeviceInfoService/src/DeviceInfoServer.c
@@ -4,8 +4,8 @@
 
 #include <ws4d-gSOAP/dpws_device.h>
 
-extern int (*send_buf)(struct dpws_s *, uint16_t , uint8_t , struct soap* , u_char* , ssize_t );
-extern ssize_t (*rcv_buf)(struct dpws_s *device, uint16_t service_id, uint8_t op_id, struct soap* msg, char **buf);
+int send_buf(struct dpws_s *, uint16_t , uint8_t , struct soap* , u_char* , ssize_t );
+ssize_t rcv_buf(struct dpws_s *device, uint16_t service_id, uint8_t op_id, struct soap* msg, char **buf);
 
 #include "DeviceInfo_operations.h"
 #include "Conversion.h"
@@ -16,13 +16,13 @@ static int soap_serve_GetDeviceInfo(struct soap *soap) //TODO: pass device conte
 	int service_id = SRV_DeviceInfo;
 	struct dpws_s *device = NULL;
 
-	(*send_buf)(device, service_id, op_id, soap, NULL, 0);
+	send_buf(device, service_id, op_id, soap, NULL, 0);
 
 	/* prepare response */
 	{
 		const char* To = wsa_header_get_ReplyTo(soap);
 		char * MessageId = wsa_header_get_MessageId(soap);
-		char * Action = "http://www.teco.edu/DeviceInfoService/GetDeviceInfoOut";
+		char * Action = "http://www.teco.edu/DeviceInfoService/GetNodeStatusOut";
 
 		soap->omode |= SOAP_IO_CHUNK;
 
@@ -69,7 +69,7 @@ static int soap_serve_GetDeviceInfo(struct soap *soap) //TODO: pass device conte
 	{
 		DeviceInfo info;
 		DeviceInfo * info_ptr = &info;
-		ssize_t len = (*rcv_buf)(device, service_id, op_id, soap, (char**)&info_ptr);
+		ssize_t len = rcv_buf(device, service_id, op_id, soap, (char**)&info_ptr);
 
 		if (len < 0) {
 			soap->error = soap_receiver_fault(soap, "No reply from Node", NULL);
@@ -95,13 +95,13 @@ static int soap_serve_StopDevice(struct soap *soap)
 	int service_id = SRV_DeviceInfo;
 	struct dpws_s *device = NULL;
 
-	(*send_buf)(device, service_id, op_id, soap, NULL, 0);
+	send_buf(device, service_id, op_id, soap, NULL, 0);
 
 	/* prepare response */
 	{
 		const char* To = wsa_header_get_ReplyTo(soap);
 		char * MessageId = wsa_header_get_MessageId(soap);
-		char * Action = "http://www.teco.edu/DeviceInfoService/StopDeviceOut";
+		char * Action = "http://www.teco.edu/DeviceInfoService/StopNodeOut";
 
 		soap->omode |= SOAP_IO_CHUNK;
 
@@ -147,7 +147,7 @@ static int soap_serve_StopDevice(struct soap *soap)
 
 	{
 		char * buf;
-		ssize_t len = (*rcv_buf)(device, service_id, op_id, soap, &buf);
+		ssize_t len = rcv_buf(device, service_id, op_id, soap, &buf);
 
 		if (len < 0) {
 			soap->error = soap_receiver_fault(soap, "No reply from Node", NULL);
@@ -170,18 +170,23 @@ static int soap_serve_StopDevice(struct soap *soap)
 int DeviceInfoService_serve_request(struct soap *soap)
 {
 	soap_peek_element(soap);
+	if (soap->action)
+	{
+		printf("Calling DeviceInfoService_serve_request with soap action %s\n",soap->action);
+	} else {
+		printf("Calling DeviceInfoService_serve_request without soap action\n");
+	}
+	if ((soap->action && !strcmp(soap->action,
+			"http://www.teco.edu/DeviceInfoService/GetNodeStatusIn"))
+
+	)
+		return soap_serve_GetDeviceInfo(soap);
 
 	if ((soap->action && !strcmp(soap->action,
-				"http://www.teco.edu/DeviceInfoService/GetDeviceInfoIn"))
+			"http://www.teco.edu/DeviceInfoService/StopNodeIn"))
 
-		)
-			return soap_serve_GetDeviceInfo(soap);
-
-		if ((soap->action && !strcmp(soap->action,
-				"http://www.teco.edu/DeviceInfoService/StopDeviceIn"))
-
-		)
-			return soap_serve_StopDevice(soap);
+	)
+		return soap_serve_StopDevice(soap);
 
 	return soap->error = SOAP_NO_METHOD;
 }
